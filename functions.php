2b19c498cfbc6187a54da4506af0dcc11dfae6d4
matthieuLabaune <matthieu.labaune@le-campus-numerique.fr>
2020-02-04 16:12:51 +0100

    fonction products index

--- functions.php
+++ functions.php
@@ -1,1 +1,15 @@
 <?php
+/**
+ * @param $var
+ */
+function debug($var)
+{
+    highlight_string("<?php\n\n" . var_export($var, true) . ";\n?>");
+}
+
+function newProducts(PDO $mabdd)
+{
+    $query = $mabdd->query('SELECT id, name, description, price FROM products ORDER BY arrivale_date DESC LIMIT 6');
+    $response = $query->fetchAll(PDO::FETCH_ASSOC);
+    return $response;
+}
--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
+include 'config.php';
 include 'functions.php';
+include 'header.php';
+include 'footer.php' ;
 
-
+$new_products = newProducts(boutiquefmm) ;
 ?>
 
-<h1>Mon nom de boutique</h1>
+    <h1>Site boutique</h1>
+
+<?php
+
+foreach ($new_products as $new_product) {
+    echo '<a href="product.php?id=' . $new_product['id'] . ' "><h2>' . $new_product['name'] . '  </h2></a>';
+    echo '<p>' . $new_product['description'] . '</p>';
+}
