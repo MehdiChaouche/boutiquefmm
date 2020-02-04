de1de0cf1ca53894436b1bb8612924378764f330
MehdiChaouche <mehdi.chaouche@le-campus-numerique.fr>
2020-02-04 16:37:03 +0100

    Header/Footer done

--- headerfooter/header.php
+++ headerfooter/header.php
@@ -1,8 +1,17 @@
 <header>
-    <nav>
-        <ul>
-            <a href="index.php?page=accueil" class="btn">Accueil</a>
-            <!-- Menu de navigation --->
-        </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="index.php?page=accueil">Accueil</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php?page=accueil">Autre page <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+        </div>
     </nav>
 </header>
